eff5b47838b8f07737c3858a68392fb41549e539
mostofa89 <mostofakamaljoy89@gmail.com>
2024-08-18 00:02:58 +0600

    Add premative Type of Java

---
+++ JavaFundamentals/premative.java
@@ -0,0 +1,12 @@
+// Example of Some Premative Types
+public class premative {
+    public static void main(String[] args) {
+        int Id = 23101454;
+        long location = 2310_863_8978_766L;
+        float price = 30.69F;
+        System.out.println(Id);
+        System.out.println(location);
+        System.out.println(price);
+    }
+
+}
