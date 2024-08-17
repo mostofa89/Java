6de72d9562aea57d7195216f340e48a79e87e913
mostofa89 <mostofakamaljoy89@gmail.com>
2024-08-18 00:10:22 +0600

    Add Variable File in JavaFundamentals

---
+++ JavaFundamentals/Variable.java
@@ -0,0 +1,19 @@
+// Variable in Java
+public class Variable {
+    public static void main(String[] args) {
+        int num = 100;
+        float cgpa = 3.65f;
+        boolean flag = true;
+        System.out.println("Before Update");
+        System.out.println(num);
+        System.out.println(cgpa);
+        System.out.println(flag);
+        num = 300;
+        cgpa = 3.69f;
+        flag = false;
+        System.out.println("After Update");
+        System.out.println(num);
+        System.out.println(cgpa);
+        System.out.println(flag);
+    }
+}
