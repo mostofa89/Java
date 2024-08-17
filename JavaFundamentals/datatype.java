17537944060f736f623d1bfa951193a3b12908be
mostofa89 <mostofakamaljoy89@gmail.com>
2024-08-17 23:51:03 +0600

    Add Java Fundamentals

---
+++ JavaFundamentals/datatype.java
@@ -0,0 +1,24 @@
+public class datatype{
+    public static void main(String[] args) {
+        int num = 123;
+        double num1 = 123.89;
+        boolean flag = true;
+        short s1 = 1000;
+        long l1 = 100000066;
+        float num2 = 16.66f;
+        char ch = 'a';
+        String words = "Hello, World!";
+        byte val = 20;
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(l1);
+        System.out.println(s1);
+        System.out.println(val);
+        System.out.println(ch);
+        System.out.println(flag);
+        System.out.print(words + " ");
+        System.out.println(flag);
+               
+    }
+}
