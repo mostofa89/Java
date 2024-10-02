40c38ba6418f59855a8d027dab572e037610cfe5
mostofa89 <mostofakamaljoy89@gmail.com>
2024-10-02 20:20:09 +0600

    Add Indexing and Slicing of String

---
+++ Fundamentals/String/indexing_Slicing.java
@@ -0,0 +1,9 @@
+package Fundamentals.String;
+
+public class indexing_Slicing {
+    public static void main(String[] args){
+        String str1 = "Hello World!";
+        System.out.println(str1.charAt(6));
+        System.out.println(str1.substring(0, str1.length() - 4));
+    }
+}
