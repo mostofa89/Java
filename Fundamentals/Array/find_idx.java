18cbddb0837130d5be182c9a4083799e40e9c101
mostofa89 <mostofakamaljoy89@gmail.com>
2024-10-05 19:43:40 +0600

    Add Index Finder in Array

---
+++ Fundamentals/Array/find_idx.java
@@ -0,0 +1,23 @@
+package Fundamentals.Array;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class find_idx {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int [] nums = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        System.out.println("Array is : " + Arrays.toString(nums));
+        System.out.print("Enter a Number : ");
+        int num = input.nextInt();
+        int idx = -1;
+        for (int i = 0; i < nums.length; i += 1){
+            if (nums[i] == num){
+                idx = i;
+                break;
+            }
+        }
+        System.out.println("Index of " + num + " is : " + idx + ".");
+    }
+}
