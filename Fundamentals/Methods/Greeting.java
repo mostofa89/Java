c5b25d485f168a22d4794bcfacee863d4132ecde
mostofa89 <mostofakamaljoy89@gmail.com>
2024-10-06 16:42:06 +0600

    Add Greeting Method

---
+++ Fundamentals/Methods/Greeting.java
@@ -0,0 +1,12 @@
+package Fundamentals.Methods;
+
+class Greeting{
+    public static void main(String[] args){
+        sayHello("Mostofa");
+        
+    }
+    
+    public static void sayHello(String name){
+        System.out.println("Hello, " + name + "!");
+    }
+}
