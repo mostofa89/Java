43b516ff52b68c5cf6d6c737316a1cbd46e89649
mostofa89 <mostofakamaljoy89@gmail.com>
2024-10-27 15:54:29 +0600

    Add Triangle and It's Driver Code

---
+++ OOP/Encapsulation/Triangle.java
@@ -0,0 +1,44 @@
+package OOP.Encapsulation;
+
+public class Triangle {
+    private int base;
+    private int height;
+    private float area;
+
+
+    public Triangle(int base, int height){
+        this.base = base;
+        this.height = height;
+        this.area = 0;
+        this.CalcArea(base, height);
+    }
+
+    
+    public void setBase(int new_base){
+        this.base = new_base;
+    }
+
+
+    public int getBase(){
+        return this.base;
+    }
+
+    public void setHeight(int new_height){
+        this.height = new_height;
+    }
+
+
+    public int getHeight(){
+        return this.height;
+    }
+
+    public float getArea(){
+        return this.area;
+    }
+
+
+    private void CalcArea(int base, int height){
+        this.area = (float) ((float) 1/2 * base * height);
+    }
+
+}
---
+++ OOP/Encapsulation/TriangleDriverCode.java
@@ -0,0 +1,16 @@
+package OOP.Encapsulation;
+
+public class TriangleDriverCode {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle(10, 5);
+        System.out.println("First Triangle Base: " + t1.getBase());
+        System.out.println("First Triangle Height: " + t1.getHeight());
+        System.out.println("First Triangle Area: " + t1.getArea());
+        System.out.println("========================================");
+        Triangle t2 = new Triangle(5, 3);
+        System.out.println("Second Triangle Base: " + t2.getBase());
+        System.out.println("Second Triangle Height: " + t2.getHeight());
+        System.out.println("Second Triangle Area: " + t2.getArea());
+
+    }
+}
