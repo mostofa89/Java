e508c5cc515f90f27409ca6a5523a77481bcbe53
mostofa89 <mostofakamaljoy89@gmail.com>
2024-10-27 15:04:11 +0600

    Add Circle and It's Driver code

---
+++ OOP/Encapsulation/Circle.java
@@ -0,0 +1,31 @@
+package OOP.Encapsulation;
+
+public class Circle {
+    private int radius;
+    private float area;
+
+
+    public Circle(int radius){
+        this.radius = radius;
+        this.area = 0f;
+    }
+
+
+    public void CalcArea(){
+        this.area = (float) (3.14f * Math.pow(this.radius, 2));
+    }
+
+
+    public float getArea(){
+        return this.area;
+    }
+
+    public int getRadius(){
+        return this.radius;
+    }
+
+    public void SetRedius(int radius){
+        this.radius = radius;
+    }
+
+}
---
+++ OOP/Encapsulation/CircleDriverCode.java
@@ -0,0 +1,14 @@
+package OOP.Encapsulation;
+
+public class CircleDriverCode {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(4);
+        System.out.println("First circle radius:" +  c1.getRadius());
+        System.out.println("First circle area:" + c1.getArea());
+        System.out.println("===============================");
+        Circle c2 = new Circle(5);
+        System.out.println("Second circle radius:" +  c2.getRadius());
+        System.out.println("Second circle area:" + c2.getArea());
+
+    }
+}
